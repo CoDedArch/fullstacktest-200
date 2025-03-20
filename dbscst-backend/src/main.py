343387ba3dccfd59eeb77a18d4ec3ff7653757f8
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2025-03-20 23:20:06 +0000

    fixed CORS issue

--- dbscst-backend/src/main.py
+++ dbscst-backend/src/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from core.database import session_manager
-from apps.databaseSchemas.models import Base
 from apps.databaseSchemas.router import router as schema_router
 from apps.auth.router import router as auth_router
 
@@ -13,20 +12,17 @@ app = FastAPI(
     on_startup=[session_manager.init_db],
 )
 
-# Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins (replace with your frontend URL in production)
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all methods (including OPTIONS)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
 
 @app.get("/", tags=["Home"], response_model=dict)
 async def home():
     return {"message": "Welcome to ..."}
-
 
 app.include_router(schema_router, tags=["schema endpoints"])
 app.include_router(auth_router, tags=["Authentication"])
