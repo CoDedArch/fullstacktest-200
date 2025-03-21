79f00528235a674a63419ca4ec181929c0d559a4
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2025-03-21 01:00:19 +0000

    contains constants

---
+++ dbscst-frontend/src/shared/constants/constants.tsx
@@ -0,0 +1,8 @@
+export const CHECK_EMAIL_URL = "http://127.0.0.1:8000/auth/check-email";
+export const SIGN_UP_URL = "http://127.0.0.1:8000/auth/signup";
+export const LOGIN_URL = "http://127.0.0.1:8000/auth/login";
+export const USER_PROJECTS_URL = "http://127.0.0.1:8000/api/user-projects";
+export const GENERATE_SCHEMA_URL = "http://127.0.0.1:8000/api/generate-schema";
+export const SINGLE_PROJECT_URL = "http://127.0.0.1:8000/api/user-projects/project/get/";
+export const PROJECT_SCHEMA_UPDATE_URL = "http://127.0.0.1:8000/api/user-projects/project/update/";
+export const API_KEY_VERIFICATION = process.env.REACT_APP_API_KEY;
