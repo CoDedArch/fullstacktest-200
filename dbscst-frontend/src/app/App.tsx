0948df1710bc75a1bcc680fa9496b174e37e14bb
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2025-03-21 01:05:35 +0000

    route project url

--- dbscst-frontend/src/app/App.tsx
+++ dbscst-frontend/src/app/App.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../ui/home";
 import Project from "../ui/project";
+import ProjectPage from "../ui/projectPage";
 
 function App() {
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/new-project" element={<Project />} />
+        <Route path="/projects/:projectId" element={<ProjectPage />} />
       </Routes>
     </Router>
-   
-  )
+  );
 }
 
-export default App
+export default App;
